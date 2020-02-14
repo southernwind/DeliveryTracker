60863c30ef752a2a6e4626b9c45248ea6184df53
southernwind <testakina@gmail.com>
2020-02-14 16:20:58 +0900

    ヤマト運輸追加

--- DeliveryTracker/Institutions/Yamato.cs
+++ DeliveryTracker/Institutions/Yamato.cs
@@ -10,18 +10,16 @@ namespace DeliveryTracker.Institutions {
 		public static TrackingType TrackingType = TrackingType.Yamato;
 
 		public static async Task<bool> IsTrackingNumberAsync(string number) {
-			return false;
-			var html = await HttpClientWrapper.GetDocumentAsync($"https://trackings.post.japanpost.jp/services/srv/search/direct?reqCodeNo1={number}&locale=ja");
-			return html.DocumentNode.QuerySelector("table[summary='履歴情報']") != null;
+			var html = await HttpClientWrapper.GetDocumentAsync($"http://jizen.kuronekoyamato.co.jp/jizen/servlet/crjz.b.NQ0010?id={number}");
+			return html.DocumentNode.QuerySelector("table.meisai") != null;
 		}
 
 		public static async Task<DeliveryStatus> GetCurrentStatus(string number) {
-			var html = await HttpClientWrapper.GetDocumentAsync($"https://trackings.post.japanpost.jp/services/srv/search/direct?reqCodeNo1={number}&locale=ja");
-			var history = html.DocumentNode.QuerySelector("table[summary='履歴情報']").QuerySelectorAll("tr");
-			var current = history.Reverse().Skip(1).FirstOrDefault();
-			var tds = current.QuerySelectorAll("td");
+			var html = await HttpClientWrapper.GetDocumentAsync($"http://jizen.kuronekoyamato.co.jp/jizen/servlet/crjz.b.NQ0010?id={number}");
+			var history = html.DocumentNode.QuerySelector("table.meisai").QuerySelectorAll("tr");
+			var tds = history.Last().QuerySelectorAll("td");
 
-			return new DeliveryStatus(tds[1].InnerText, DateTime.Parse(tds[0].InnerText));
+			return new DeliveryStatus(tds[1].InnerText, DateTime.Parse($"{DateTime.Now.Year}/{tds[2].InnerText} {tds[3].InnerText}:00"));
 		}
 	}
 }
